83f6bc196d17637dfce69d2a9ae7ed63d1db175a
Rub21 <ruben@mapbox.com>
2015-05-14 10:27:05 +0530

    add status

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var csv = require('csv-parser')
 var _ = require('underscore');
 var OSRM = require('osrm-client');
 var osrm = new OSRM("http://router.project-osrm.org/");
-var url = "http://mape.project-osrm.org/?hl=en&loc=";
-var obj_routing = {};
-
+var url = "http://map.project-osrm.org/?hl=en&loc=";
 var spreadsheets = [];
 var world = JSON.parse(fs.readFileSync('world.geojson', 'utf8'));
-fs.writeFile("link-routes.csv", "ID | Map evaluation | API evaluation | url_start_via_end |	url_start_via | url_via_end+ \n", function(err) {});
+fs.writeFile("link-routes.csv", "ID | Map evaluation | API evaluation | url_start_via_end |	url_start_via | url_via_end | status | issue description \n", function(err) {});
 var rqt = fs.createReadStream('feedbacks.csv')
 	.pipe(csv())
 	.on('data', function(data) {
@@ -19,7 +17,7 @@ var rqt = fs.createReadStream('feedbacks.csv')
 			"notes": {},
 			"created_at": data.created_at,
 			"updated_at": data.updated_at
-		}
+		};
 		var array_notes = data.notes.replace(/\n/g, "*#").split("*#");
 		var notes = {
 			'Unroutable _Waypoint': array_notes[0].split(":")[1],
@@ -31,7 +29,7 @@ var rqt = fs.createReadStream('feedbacks.csv')
 			'Byway': array_notes[6].split(":")[1],
 			waypoint_before: {},
 			waypoint_after: {}
-		}
+		};
 		var waypoint_before = {
 			'Type': null,
 			'Name': null,
@@ -52,7 +50,6 @@ var rqt = fs.createReadStream('feedbacks.csv')
 			if (array_notes[i] !== undefined) {
 				var arr = array_notes[i].split(":");
 				if (arr.length > 1) {
-
 					if (i === 11) {
 						waypoint_before[arr[0].toString()] = arr[1].replace("[", "").replace("]", "").replace(" ", "").split(",");
 
@@ -83,33 +80,32 @@ var rqt = fs.createReadStream('feedbacks.csv')
 	});
 
 rqt.on('finish', function() {
-	var text = ""
 	_.each(spreadsheets, function(element) {
-
 		var coor_start = element.notes.waypoint_before.Location;
 		var coor_via = element.notes.Location;
 		var coor_end = element.notes.waypoint_after.Location;
-		var bandera_arr = false;
+		var isrouting_continents = false;
 		_.each(world.features, function(val) {
-			var v = (coor_end.length > 0 ? pointinpolygon(coor_start, val.geometry.coordinates) : true) && (coor_via.length > 0 ? pointinpolygon(coor_via, val.geometry.coordinates) : true) && (coor_end.length > 0 ? pointinpolygon(coor_end, val.geometry.coordinates) : true);
-			bandera_arr = bandera_arr || v;
+			var v = (coor_start.length > 0 ? pointinpolygon(coor_start, val.geometry.coordinates) : true) && (coor_via.length > 0 ? pointinpolygon(coor_via, val.geometry.coordinates) : true) && (coor_end.length > 0 ? pointinpolygon(coor_end, val.geometry.coordinates) : true);
+			isrouting_continents = isrouting_continents || v;
 		});
 
 		coor_start = element.notes.waypoint_before.Location.reverse();
 		coor_via = element.notes.Location.reverse();
 		coor_end = element.notes.waypoint_after.Location.reverse();
-
+		var status = "";
 		var routing_continents = "";
-		if (!bandera_arr) {
+		if (!isrouting_continents) {
 			routing_continents = "No Routing";
+			status = "won't fix";
 		}
 
 		var url_routing = "";
 		var query = {};
-		if (coor_end == '') {
+		if (coor_end.length === 0) {
 			var url_start_via = url + coor_start + "&loc=" + coor_via;
-			url_start_via = '=HYPERLINK("' + url_start_via + '","url_start_via")';
-			url_routing = "|-- " + "| " + url_start_via + "|-- ";
+			url_start_via = '=HYPERLINK("' + url_start_via + '","Start-Via")';
+			url_routing = "|-- | " + url_start_via + "|-- ";
 			query = {
 				coordinates: [
 					coor_start,
@@ -118,13 +114,13 @@ rqt.on('finish', function() {
 			};
 		} else {
 			var url_start_via_end = url + coor_start + "&loc=" + coor_via + "&loc=" + coor_end;
-			url_start_via_end = '=HYPERLINK("' + url_start_via_end + '","url_start_via_end")';
+			url_start_via_end = '=HYPERLINK("' + url_start_via_end + '","Start-Via-End")';
 
 			var url_start_via = url + coor_start + "&loc=" + coor_via;
-			url_start_via = '=HYPERLINK("' + url_start_via + '","url_start_via")';
+			url_start_via = '=HYPERLINK("' + url_start_via + '","Start-Via")';
 
 			var url_via_end = url + coor_via + "&loc=" + coor_end;
-			url_via_end = '=HYPERLINK("' + url_via_end + '","url_via_end")';
+			url_via_end = '=HYPERLINK("' + url_via_end + '","Via-End")';
 			url_routing = "| " + url_start_via_end + "| " + url_start_via + "| " + url_via_end;
 			query = {
 				coordinates: [
@@ -136,22 +132,16 @@ rqt.on('finish', function() {
 		}
 
 		osrm.route(query, function(err, result) {
-			console.log(result.route_summary);
 			if (result.route_summary !== undefined) {
-				var t = element.id + " | " + routing_continents + "|  " + url_routing + "\n";
+				status = "fixed";
+				var t = element.id + " | " + routing_continents + "|  Routing  " + url_routing + "| " + status + " | \n";
 				fs.appendFile('link-routes.csv', t, function(err) {});
-
 			} else {
-				var t = element.id + " | " + routing_continents + "|  No Routing " + url_routing + "\n";
+				var t = element.id + " | " + routing_continents + "|  No Routing " + url_routing + "| " + status + " | \n";
 				fs.appendFile('link-routes.csv', t, function(err) {});
 			}
-
 		});
-
 	});
-
-
-
 });
 
 function pointinpolygon(point, vs) {
